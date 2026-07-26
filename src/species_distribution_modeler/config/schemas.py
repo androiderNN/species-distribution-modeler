438dc015bbd42e7bf74a55926c76aaa27ddea6c0
Kento <kento.i.1103@gmail.com>
2026-07-26 23:06:15 +0900

    DSMでなくDEMだったので修正

--- src/species_distribution_modeler/config/schemas.py
+++ src/species_distribution_modeler/config/schemas.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 
 class PathConfig(BaseModel):
     """各種パス"""
-    dsm_dir: Path
+    dem_dir: Path
     lulc_dir: Path
 
 
---
+++ src/species_distribution_modeler/dataset/__init__.py
@@ -0,0 +1,3 @@
+from . import jaxa_lulc, gsi_dem, feature_extractor
+
+__all__ = ["jaxa_lulc", "gsi_dem", "feature_extractor"]
